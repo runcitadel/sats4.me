77b4ae2d0994544396f1c5c45e8e1805cdb3d700
AaronDewes <aaron.dewes@protonmail.com>
2022-10-21 19:50:55 +0000

    Check if user exists

--- routes/[name].tsx
+++ routes/[name].tsx
@@ -1,29 +1,47 @@
 import { Head } from "$fresh/runtime.ts";
-import { PageProps } from "$fresh/server.ts";
+import { Handlers, PageProps } from "$fresh/server.ts";
 import SendLn from "../islands/SendLn.tsx";
 import SendOnChain from "../islands/SendOnChain.tsx";
+import * as mainLogic from "../src/manage.ts";
 
-export default function SendSats(props: PageProps) {
+interface SendSatsProps {
+  exists: boolean;
+}
+
+export const handler: Handlers<SendSatsProps> = {
+  async GET(_, ctx) {
+    const { name } = ctx.params;
+    const proxyTarget = await mainLogic.getProxyTarget(name);
+
+    return ctx.render({
+      exists: !!proxyTarget,
+    });
+  },
+};
+
+export default function SendSats(props: PageProps<SendSatsProps>) {
   return (
     <>
-      <Head>
-        <title>Sats for {props.params.name}</title>
-        <meta property="twitter:card" content="summary" />
-        <meta
-          property="og:site_name"
-          content={`Sats for @${props.params.name}`}
-        />
-        <meta
-          name="lightning"
-          content={`lnurlp:${props.params.name}@sats4.me`}
-        />
-        <meta
-          property="og:description"
-          content={`Send some Sats to @${props.params.name} - powered by sats4me.`}
-        />
+      {props.data.exists && (
+        <Head>
+          <title>Sats for {props.params.name}</title>
+          <meta property="twitter:card" content="summary" />
+          <meta
+            property="og:site_name"
+            content={`Sats for @${props.params.name}`}
+          />
+          <meta
+            name="lightning"
+            content={`lnurlp:${props.params.name}@sats4.me`}
+          />
+          <meta
+            property="og:description"
+            content={`Send some Sats to @${props.params.name} - powered by sats4me.`}
+          />
 
-        <meta property="og:image" content="/lnme.svg" />
-      </Head>
+          <meta property="og:image" content="/lnme.svg" />
+        </Head>
+      )}
       <div
         class="h-screen w-screen flex flex-col text-[8vh] items-center justify-center text-center text-white"
         style={{
@@ -31,9 +49,14 @@ export default function SendSats(props: PageProps) {
             "linear-gradient(60deg, #3d3393 0%, #2b76b9 37%, #2cacd1 65%, #35eb93 100%)",
         }}
       >
-        <SendLn user={props.params.name} />
-
-        <SendOnChain user={props.params.name} />
+        {props.data.exists
+          ? (
+            <>
+              <SendLn user={props.params.name} />
+              <SendOnChain user={props.params.name} />
+            </>
+          )
+          : <p class="text-2xl">This user does not exist.</p>}
       </div>
     </>
   );
