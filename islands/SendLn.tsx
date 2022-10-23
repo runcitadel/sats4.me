d06b705abed75c33276efbf22d7433e7ae35d9b4
AaronDewes <aaron.dewes@protonmail.com>
2022-10-23 13:10:53 +0000

    Improve sendLn

--- islands/SendLn.tsx
+++ islands/SendLn.tsx
@@ -9,47 +9,74 @@ export default function SendLn(props: SendLnProps) {
   const [amount, setAmount] = useState<number | undefined>();
   const [memo, setMemo] = useState<string | undefined>();
   const [invoice, setInvoice] = useState<string | undefined>();
+  const [paid, setPaid] = useState(false);
   async function sendSats() {
-    const res = await fetch(`/callback/${encodeURIComponent(props.user)}?amount=${amount}&memo=${memo}`);
+    const res = await fetch(
+      `/callback/${
+        encodeURIComponent(props.user)
+      }?amount=${amount}&memo=${memo}`,
+    );
     const invoice = (await res.json()).pr;
     try {
       const webln = await requestProvider();
       await webln.enable();
       await webln.sendPayment(invoice);
+      setPaid(true);
     } catch {
       setInvoice(invoice);
     }
   }
   return (
-    {invoice ? <p>Thank you! <a href={`lightning:${invoice}`}>{invoice}</a></p> : <p>
-      Send me<br />
-      <input
-        type="number"
-        placeholder="10000"
-        class="appearance-textfield bg-transparent outline-none border-b-1 border-white w-[5em] max-w-full text-center"
-        autofocus
-        autocomplete="off"
-        min="1"
-        value={amount}
-        onChange={(event) => {
-          setAmount(Number((event.target! as HTMLInputElement).value));
-        }}
-      />{" "}
-      Sats
-      <br />
-      for
-      <br />
-      <input
-        type="text"
-        class="appearance-textfield bg-transparent outline-none border-b-1 border-white w-[5em] max-w-full text-center"
-        placeholder="message"
-        autocomplete="off"
-        value={memo}
-        onChange={(event) => {
-          setMemo((event.target! as HTMLInputElement).value);
-        }}
-      />
-      <button class="rounded-full py-2 px-8 bg-blue-500 text-3xl w-5/6 mt-4" onClick={() => sendSats()}>⚡ Send ⚡</button>
-    </p>}
+    <p>
+      {invoice
+        ? (
+          <>
+            Thank you! <a href={`lightning:${invoice}`}>{invoice}</a>
+          </>
+        )
+        : (
+          <>
+            {paid ? <>Thank you!</> : (
+              <>
+                Send me<br />
+                <input
+                  type="number"
+                  placeholder="10000"
+                  class="appearance-textfield bg-transparent outline-none border-b-1 border-white w-[5em] max-w-full text-center"
+                  autofocus
+                  autocomplete="off"
+                  min="1"
+                  value={amount}
+                  onChange={(event) => {
+                    setAmount(
+                      Number((event.target! as HTMLInputElement).value),
+                    );
+                  }}
+                />{" "}
+                Sats
+                <br />
+                for
+                <br />
+                <input
+                  type="text"
+                  class="appearance-textfield bg-transparent outline-none border-b-1 border-white w-[5em] max-w-full text-center"
+                  placeholder="message"
+                  autocomplete="off"
+                  value={memo}
+                  onChange={(event) => {
+                    setMemo((event.target! as HTMLInputElement).value);
+                  }}
+                />
+                <button
+                  class="rounded-full py-2 px-8 bg-blue-500 text-3xl w-5/6 mt-4"
+                  onClick={() => sendSats()}
+                >
+                  ⚡ Send ⚡
+                </button>
+              </>
+            )}
+          </>
+        )}
+    </p>
   );
 }
